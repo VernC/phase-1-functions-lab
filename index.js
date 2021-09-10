f2de4174930f214d0ba72cbdcd84ca80d8392228
Candice <candice.vernacchio@gmail.com>
2021-09-09 18:22:39 -0600

    commit tree 26c46c4d8273d915aee39806c564cd8887676202

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+let headQuarters = 42;
+let blockInFeet = 264
+
+function distanceFromHqInBlocks(pickUpLocation) {
+    return Math.abs (pickUpLocation - headQuarters)
+}
+
+function distanceFromHqInFeet(pickUpLocation) {
+    return Math.abs ((pickUpLocation - headQuarters) * blockInFeet)
+}
+
+function distanceTravelledInFeet(pickUpLocation, dropOffLocation) {
+    return Math.abs ((pickUpLocation - dropOffLocation) * blockInFeet)
+}
+
+let calculatesFarePrice = (pickUpLocation, dropOffLocation) => {
+    let farePrice = distanceTravelledInFeet (pickUpLocation, dropOffLocation);
+    if(farePrice <= 400) {
+        return 0;
+    } else if(farePrice >400 && farePrice<= 2000) {
+        return (farePrice - 400) * 0.02;
+    } else if(farePrice > 2000 && farePrice <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far"
+    }
+}
